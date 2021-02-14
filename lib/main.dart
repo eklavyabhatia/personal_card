3026e35be14fc2863cbe10f020acd9867017e8e2
eklavya bhatia <eklavyabhatia@gmail.com>
2021-02-14 23:29:38 +0530

    Create Avatar

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            children: [
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/eklavya.jpeg'),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
